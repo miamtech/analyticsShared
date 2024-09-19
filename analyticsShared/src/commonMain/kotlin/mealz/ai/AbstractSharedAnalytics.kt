891cec8b2206cfe3bc75d2b8b738d2f306008b2b
Antonin Francois <antonin.francois@mealz.ai>
2024-09-19 11:51:44 +0200

    Validate path

--- analyticsShared/src/commonMain/kotlin/mealz/ai/AbstractSharedAnalytics.kt
+++ analyticsShared/src/commonMain/kotlin/mealz/ai/AbstractSharedAnalytics.kt
@@ -24,19 +24,43 @@ abstract class AbstractSharedAnalytics {
 
     abstract fun sendRequest(event: PlausibleEvent);
 
+    private fun validatePath(path: String) {
+        // If the path does not contain "/miam/", it may just be the URL of the page (web) or
+        // an empty path (mobile) so we assume it is valid
+        if (!path.contains("/miam/")) return
+
+        val validParts = "||miam|recipes|liked|categories|my-meals|detail|replace-item|sponsor|meals-planner|catalog|results|basket-preview|finalize|"
+        var pathWithoutURL = path.substringAfter("/miam/")
+        // Not using a list is the lightest way to do it when compiled in JS
+        while (pathWithoutURL.isNotEmpty()) {
+            // Find the index of the next '/' to separate path segments
+            val nextSlashIndex = pathWithoutURL.indexOf('/').takeIf { it != -1 } ?: pathWithoutURL.length
+            val part = pathWithoutURL.substring(0, nextSlashIndex)
+            // If the part is not in the valid set of path segments, throw an exception
+            if (!validParts.contains("|$part|")) {
+                throw IllegalArgumentException("Invalid path : $part")
+            }
+
+            // Remove the processed part and move to the next segment if there's more path left
+            pathWithoutURL = if (nextSlashIndex < pathWithoutURL.length) {
+                pathWithoutURL.substring(nextSlashIndex + 1)
+            } else "" // Path completely parsed
+        }
+    }
+
     internal fun buildAndSendPlausibleRequest(eventType: String, path: String, props: PlausibleProps) {
         if (!alreadyInitialized) return
 
+        validatePath(path)
         val fullProps = props.copy(
             version = version,
             platform = getPlatform().name,
             abTestKey = abTestKey,
             affiliate = affiliate
         )
-        val fullPath = if (path.isNotBlank()) "miam$path" else ""
         val event = PlausibleEvent(
             eventType,
-            fullPath,
+            path,
             domain,
             fullProps
         )
